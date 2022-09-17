b8e7f581d7e0b917ace309ae3a3d38d87e14c83e
MosNes <7hekarl@gmail.com>
2022-09-17 15:37:52 -0400

    installed react and reactstrap

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+
+      </header>
+      <main>
+      
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
