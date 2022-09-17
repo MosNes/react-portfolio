8439827956bffb6a19ff042e8e38a5f03489dfa1
MosNes <7hekarl@gmail.com>
2022-09-17 17:51:05 -0400

    added placeholder files for pages, updated About page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import { } from 'reactstrap';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App bg-dark text-light">
       <Header/>
       <main className='container-fluid'>
-       text
+        <About />
       </main>
       <Footer />
     </div>
---
+++ client/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+const About = () => {
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <img alt="J Moses Nester" className="bioPic" src={require('../assets/images/Headshot.jpg')} />
+                </Col>
+                <Col className="bodyText">
+                    <h3>About Me</h3>
+                    <p>Bro ipsum dolor sit amet punter ski bum rock roll frozen chicken heads single track rig. Washboard schraeder liftie, glades gnar slash schwag steed. Free ride T-bar acro trucks fully hammer crunchy pillow popping, bonk back country corn hardtail. Ripping rigid trail swag 360 pipe huck. Tele butter gnar drop. Ski bum reverse camber backside glades bear trap schwag bowl face shots taco mute ripper noodle line skid poaching. Poaching deck greasy bonk, sharkbite reverse camber park giblets scream berm hammerhead moguls stoked butter wheelie.</p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default About;
