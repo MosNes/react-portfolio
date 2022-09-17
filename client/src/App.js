6be2882685272d4ff93ea4fd95dfc5179a3d6aa2
MosNes <7hekarl@gmail.com>
2022-09-17 19:25:57 -0400

    added helpers.js with validate email function, created Contact component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Header/>
       <main className='container-fluid'>
         {/* <About /> */}
-        <Portfolio />
+        {/* <Portfolio /> */}
+        <Contact />
       </main>
       <Footer />
     </div>
---
+++ client/src/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {  } from 'reactstrap';
+
+const Contact = () => {
+    return(
+        <div>This is some text</div>
+    )
+};
+
+export default Contact;
