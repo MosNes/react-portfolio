3ea1c609d08774b843eb237b177ee675fe0f6c53
MosNes <7hekarl@gmail.com>
2022-09-17 16:52:46 -0400

    added style.css, finished styling footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,13 @@ const Footer = (props) => {
 
     return(
         <footer className="bg-secondary text-light container-fluid">
-            <div className='d-flex p-2'>
+            <div className='d-flex p-2 justify-content-center'>
+                <a href="https://github.com/MosNes" target="blank" >
+                    <img alt="Github Logo" className='footerImg' src={require('../../assets/images/Github-Logo-White.png')}></img>
+                </a>
+                <a href="https://www.linkedin.com/in/moses-nester-8515b565" target="blank" >
+                    <img alt="LinkedIn Logo" className='footerImg' src={require('../../assets/images/linkedin-logo-white-1024x1024.png')}></img>
+                </a>
                 
             </div>
         </footer>
